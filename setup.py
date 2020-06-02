8b21f1e63a2f86496123ece34c11b0fbeb0c52e1
cyy <cyyever@outlook.com>
2020-06-03 01:14:32 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cyy_naive_lib",
+    author="cyy",
+    version="0.1",
+    author_email="cyyever@outlook.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyyever/naive_python_lib",
+    packages=["cyy_naive_lib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
