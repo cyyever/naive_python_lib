7fa736f47e23409756034f8def14403ef1974110
cyy <cyyever@outlook.com>
2020-07-19 19:36:30 +0800

    add code

--- cyy_naive_lib/test/test_list_op.py
+++ cyy_naive_lib/test/test_list_op.py
@@ -1,13 +1,13 @@
-from list_op import split_list_to_chunks, dict_values_to_list
+from list_op import split_list_to_chunks, dict_value_by_order
 
 
 def test_split_list_to_chunks():
-    res = split_list_to_chunks(list(range(5)), 2)
+    res = list(split_list_to_chunks(list(range(5)), 2))
     assert len(res) == 3
     assert res[0] == [0, 1]
     assert res[2] == [4]
 
 
-def test_dict_values_to_list():
-    res = dict_values_to_list({2: "b", 1: "a"})
+def test_dict_value_by_order():
+    res = list(dict_value_by_order({2: "b", 1: "a"}))
     assert res == ["a", "b"]
---
+++ cyy_naive_lib/test/test_task_queue.py
@@ -0,0 +1,8 @@
+from task_queue import TaskQueue
+
+
+def test_task_queue():
+    queue = TaskQueue(lambda task: print("hello world"), 2)
+    queue.start()
+    queue.add_task(())
+    queue.stop()
