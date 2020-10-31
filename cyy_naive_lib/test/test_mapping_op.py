9292f8460b6044848a4c63d99d352aa839039305
cyy <cyyever@outlook.com>
2020-11-01 03:07:16 +0800

    fix code

--- cyy_naive_lib/test/test_mapping_op.py
+++ cyy_naive_lib/test/test_mapping_op.py
@@ -1,4 +1,4 @@
-from mapping_op import get_mapping_values_by_order, change_mapping_keys
+from mapping_op import get_mapping_values_by_order, change_mapping_keys, flatten_mapping
 
 
 def test_get_mapping_values_by_order():
@@ -9,3 +9,8 @@ def test_get_mapping_values_by_order():
 def test_change_mapping_keys():
     res = change_mapping_keys({2: "b", 1: "a"}, str)
     assert res == {"2": "b", "1": "a"}
+
+
+def test_flatten_mapping():
+    res = flatten_mapping({2: "b", 1: {1: "c", 2: "a"}})
+    assert res == ["c", "a", "b"]
