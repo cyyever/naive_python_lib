8d36883a0ee32f948f03669bed9f0e9a8a46525d
cyy <cyyever@outlook.com>
2020-11-13 17:43:36 +0800

    add code

--- cyy_naive_lib/shell_factory.py
+++ cyy_naive_lib/shell_factory.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+import tempfile
 from system_info import get_operating_system
 from shell.script import Script
 from shell.pwsh_script import PowerShellScript
@@ -18,8 +20,13 @@ def get_shell_script(os_hint: str = None) -> Script:
     return get_shell_script_type(os_hint)()
 
 
-def exec_cmd(cmd: str, throw: bool = True):
-    output, exit_code = get_shell_script_type()(cmd).exec(throw=False)
+def exec_cmd(cmd: str, throw: bool = True, use_temp_dir: bool = False):
+    pre_dir = os.getcwd()
+    with tempfile.TemporaryDirectory() as dir_name:
+        if use_temp_dir:
+            os.chdir(dir_name)
+        output, exit_code = get_shell_script_type()(cmd).exec(throw=False)
+        os.chdir(pre_dir)
     if throw and exit_code != 0:
         raise RuntimeError("failed to execute commands:" + str(cmd))
     return output, exit_code
