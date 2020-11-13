77a334c5cb374beca8a2b2037f47a2394c3a3507
cyy <cyyever@outlook.com>
2020-11-14 00:46:29 +0800

    fix code

--- cyy_naive_lib/tempdir.py
+++ cyy_naive_lib/tempdir.py
@@ -10,8 +10,9 @@ class TempDir:
     def __enter__(self):
         self.prev_dir = os.getcwd()
         self.temp_dir = tempfile.TemporaryDirectory()
-        os.chdir(self.temp_dir)
-        return self.temp_dir.__enter__()
+        path = self.temp_dir.__enter__()
+        os.chdir(path)
+        return path
 
     def __exit__(self, *args):
         os.chdir(self.prev_dir)
