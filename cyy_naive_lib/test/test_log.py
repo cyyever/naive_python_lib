5aed284a06a2dd7f205560bcedf9b66104862a8e
cyy <cyyever@outlook.com>
2021-03-24 01:14:22 +0800

    fix code

--- cyy_naive_lib/test/test_log.py
+++ cyy_naive_lib/test/test_log.py
@@ -1,15 +1,16 @@
 import logging
-from log import get_logger, set_file_handler
+
 from fs.tempdir import TempDir
-from util import readlines
+from log import get_logger, set_file_handler
 
 
 def test_log():
     with TempDir():
-        set_file_handler("./log")
         get_logger().debug("debug msg")
         get_logger().setLevel(logging.INFO)
         get_logger().debug("no debug msg")
         get_logger().info("info msg")
         get_logger().error("error msg")
-        get_logger().info("file content %s", readlines("./log"))
+        # set_file_handler("./log")
+        # with open("./log", "rt") as f:
+        #     get_logger().info("file content %s", f.readlines())
