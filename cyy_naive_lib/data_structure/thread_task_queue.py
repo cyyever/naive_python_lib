6561eff08170a53808591498ec119a303785c187
cyy <cyyever@outlook.com>
2021-05-27 16:33:00 +0800

    add code

--- cyy_naive_lib/data_structure/thread_task_queue.py
+++ cyy_naive_lib/data_structure/thread_task_queue.py
@@ -8,7 +8,7 @@ from .task_queue import TaskQueue
 class ThreadTaskQueue(TaskQueue):
     def __init__(
         self,
-        worker_fun: Callable,
+        worker_fun: Callable = None,
         worker_num: int = 1,
     ):
         super().__init__(worker_fun=worker_fun, ctx=threading, worker_num=worker_num)
