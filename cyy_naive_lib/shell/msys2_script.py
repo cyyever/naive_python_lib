847b49bc67f365f9927693c089fcfc6bda475665
cyy <cyyever@outlook.com>
2021-07-09 12:37:01 +0800

    fix code

--- cyy_naive_lib/shell/msys2_script.py
+++ cyy_naive_lib/shell/msys2_script.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-import os
-import sys
-
-sys.path.insert(0, os.path.join(sys.path[0], ".."))
-
-from fs.tempdir import TempDir
 
 from .bash_script import BashScript
 
 
 class MSYS2Script(BashScript):
     def _get_exec_command_line(self):
-        with TempDir():
-            with open("script.sh", "w") as f:
-                f.write(self.get_complete_content())
+        with open("script.sh", "w") as f:
+            f.write(self.get_complete_content())
             return [
                 "msys2_shell.cmd",
                 "-msys",
