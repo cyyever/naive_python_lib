fbfe13449b26deae429cd5b778c86151f3bb0463
cyy <cyyever@outlook.com>
2021-08-08 23:50:31 +0800

    fix code

--- cyy_naive_lib/algorithm/sequence_op.py
+++ cyy_naive_lib/algorithm/sequence_op.py
@@ -1,12 +1,13 @@
-from typing import Generator
+from typing import Generator, Sequence
 
 
 def split_list_to_chunks(my_list: list, chunk_size: int) -> Generator:
     r"""
     Return a sequence of chunks
     """
-    return (my_list[offs: offs + chunk_size]
-            for offs in range(0, len(my_list), chunk_size))
+    return (
+        my_list[offs: offs + chunk_size] for offs in range(0, len(my_list), chunk_size)
+    )
 
 
 def flatten_list(seq: list) -> list:
@@ -17,6 +18,19 @@ def flatten_list(seq: list) -> list:
     for x in seq:
         if isinstance(x, list):
             res += flatten_list(x)
+        else:
+            res.append(x)
+    return res
+
+
+def flatten_seq(seq: Sequence) -> list:
+    r"""
+    Flatten Return a flatted sequence
+    """
+    res = []
+    for x in seq:
+        if isinstance(x, (list, tuple)):
+            res += flatten_seq(x)
         else:
             res.append(x)
     return res
