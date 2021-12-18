40a78d1d3495758a05203e706b53968ef43d7baa
cyy <cyyever@outlook.com>
2021-12-18 17:58:08 +0800

    add code

--- cyy_naive_lib/test/data_structure/test_task_queue.py
+++ cyy_naive_lib/test/data_structure/test_task_queue.py
@@ -1,15 +1,17 @@
+from data_structure.coroutine_task_queue import CoroutineTaskQueue
 from data_structure.process_task_queue import ProcessTaskQueue
 from data_structure.thread_task_queue import ThreadTaskQueue
 
 
-def worker(*args, **kwargs):
+def worker(*args):
     print("hello world")
 
 
 def test_task_queue():
-    for queue_type in [ThreadTaskQueue, ProcessTaskQueue]:
+    for queue_type in [ThreadTaskQueue, ProcessTaskQueue, CoroutineTaskQueue]:
         queue = queue_type(worker, 2)
         queue.start()
+        queue.add_task(())
         queue.add_task(())
         queue.stop()
         queue.start()
