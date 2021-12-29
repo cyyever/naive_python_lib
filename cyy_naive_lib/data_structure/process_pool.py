a3aafc2e574ff04107cb0ccf338e6a0fa7f14874
cyy <cyyever@outlook.com>
2021-12-29 17:49:07 +0800

    fix code

--- cyy_naive_lib/data_structure/process_pool.py
+++ cyy_naive_lib/data_structure/process_pool.py
@@ -4,5 +4,7 @@ from .executor_pool import ExecutorPool
 
 
 class ProcessPool(ExecutorPool):
-    def __init__(self):
-        super().__init__(concurrent.futures.ProcessPoolExecutor())
+    def __init__(self, max_workers=None):
+        super().__init__(
+            concurrent.futures.ProcessPoolExecutor(max_workers=max_workers)
+        )
