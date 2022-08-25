57c0f51183ae6dffbc10cc27575c7c9e2f86860e
cyy <cyyever@outlook.com>
2022-08-25 21:12:38 +0800

    fix code

--- cyy_naive_lib/test/algorithm/test_sequence_op.py
+++ cyy_naive_lib/test/algorithm/test_sequence_op.py
@@ -1,5 +1,5 @@
 from cyy_naive_lib.algorithm.sequence_op import (flatten_list,
-                                                 split_list_to_chunks)
+                                                 split_list_to_chunks, sublist)
 
 
 def test_split_list_to_chunks():
@@ -14,3 +14,10 @@ def test_flatten_sequence():
     assert res == [1, 2, 3]
     res = flatten_list(["abc"])
     assert res == ["abc"]
+
+
+def test_sublist():
+    a = (1, 2, 3, 4, 5, 2)
+    b = (2,)
+    assert sublist(a, b) == 1
+    assert sublist(a, b, 3) == 5
