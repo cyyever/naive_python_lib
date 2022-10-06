1d4e9e809ac3dce3a62724d28dc1640dad662d36
cyy <cyyever@outlook.com>
2022-10-06 14:48:14 +0800

    add code

--- cyy_naive_lib/profiling.py
+++ cyy_naive_lib/profiling.py
@@ -5,24 +5,28 @@ import sys
 
 class Profile:
     def __init__(self, stats_stream=sys.stdout):
-        self.profile = None
-        self.stats_stream = stats_stream
+        self.__profile = None
+        self.__stats_stream = stats_stream
+
+    @property
+    def profile(self) -> cProfile.Profile:
+        if self.__profile is None:
+            self.__profile = cProfile.Profile()
+        return self.__profile
 
     def __enter__(self):
-        self.profile = cProfile.Profile()
         self.profile.enable()
         return self
 
     def __exit__(self, exc_type, exc_value, real_traceback):
+        self.profile.disable()
         if real_traceback:
             return
-        self.profile.disable()
-        ps = pstats.Stats(self.profile, stream=self.stats_stream).sort_stats(
+        ps = pstats.Stats(self.profile, stream=self.__stats_stream).sort_stats(
             pstats.SortKey.CUMULATIVE
         )
         ps.print_stats()
         ps.print_callers()
 
-    def save(self, path: str):
-        assert self.profile
+    def dump(self, path: str):
         self.profile.dump_stats(path)
