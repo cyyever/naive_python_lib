c249131e49a124f475199047ae4986eed4dc6138
cyy <cyyever@outlook.com>
2022-12-10 14:55:44 +0800

    fix code

--- cyy_naive_lib/reproducible_random_env.py
+++ cyy_naive_lib/reproducible_random_env.py
@@ -21,12 +21,12 @@ class ReproducibleRandomEnv:
     def __init__(self):
         self.__randomlib_state = None
         self.__numpy_state = None
-        self.__enabled = False
+        self._enabled = False
         self.__last_seed_path = None
 
     @property
     def enabled(self):
-        return self.__enabled
+        return self._enabled
 
     @property
     def last_seed_path(self):
@@ -34,7 +34,7 @@ class ReproducibleRandomEnv:
 
     def enable(self):
         with self.lock:
-            if self.__enabled:
+            if self._enabled:
                 get_logger().warning("%s use reproducible env", id(self))
             else:
                 get_logger().warning("%s initialize and use reproducible env", id(self))
@@ -53,12 +53,12 @@ class ReproducibleRandomEnv:
                 else:
                     get_logger().debug("get np random lib state")
                     self.__numpy_state = np.random.get_state()
-            self.__enabled = True
+            self._enabled = True
 
     def disable(self):
         get_logger().warning("disable reproducible env")
         with self.lock:
-            self.__enabled = False
+            self._enabled = False
 
     def __enter__(self):
         self.enable()
@@ -79,7 +79,7 @@ class ReproducibleRandomEnv:
         seed_path = os.path.join(seed_dir, "random_seed.pk")
         get_logger().warning("%s save reproducible env to %s", id(self), seed_path)
         with self.lock:
-            assert self.__enabled
+            assert self._enabled
             os.makedirs(seed_dir, exist_ok=True)
             self.__last_seed_path = seed_path
             with open(seed_path, "wb") as f:
@@ -88,17 +88,19 @@ class ReproducibleRandomEnv:
                     f,
                 )
 
-    def load(self, path: str = None, seed_dir=None) -> None:
-        assert path is None or seed_dir is None
+    def load_state(self, state: dict) -> None:
+        self.__randomlib_state = state["randomlib_state"]
+        self.__numpy_state = state["numpy_state"]
+
+    def load(self, path: str | None = None, seed_dir: str | None = None) -> None:
         if path is None:
+            assert seed_dir is not None
             path = os.path.join(seed_dir, "random_seed.pk")
         with self.lock:
-            assert not self.__enabled
+            assert not self._enabled
             with open(path, "rb") as f:
                 get_logger().warning("%s load reproducible env from %s", id(self), path)
-                obj: dict = pickle.load(f)
-                self.__randomlib_state = obj["randomlib_state"]
-                self.__numpy_state = obj["numpy_state"]
+                self.load_state(pickle.load(f))
 
     def load_last_seed(self):
         self.load(self.last_seed_path)
