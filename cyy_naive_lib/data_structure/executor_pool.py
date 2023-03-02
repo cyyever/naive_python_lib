8a1b1bff3d19434b32d1ae9479c26b2d994ffe70
cyy <cyyever@outlook.com>
2023-03-02 11:10:25 +0800

    fix code

--- cyy_naive_lib/data_structure/executor_pool.py
+++ cyy_naive_lib/data_structure/executor_pool.py
@@ -1,20 +1,40 @@
 import asyncio
+import concurrent
 import concurrent.futures
 import inspect
 import traceback
 import warnings
+# concurrent.futures.ProcessPoolExecutor(mp_context=mp_context, **kwargs)
 from typing import Callable, List
 
 from cyy_naive_lib.log import get_log_files, get_logger, set_file_handler
 
 
-class ExecutorPool:
-    def __init__(self, executor):
+class ExecutorPool(concurrent.futures._base.Executor):
+    def __init__(self, executor: concurrent.futures._base.Executor):
         self.__executor = executor
         self.__futures: List[concurrent.futures.Future] = []
 
-    def wait(self, timeout=None) -> list:
-        concurrent.futures.wait(self.__futures, timeout=timeout)
+    def submit(self, fn, /, *args, **kwargs):
+        """Submits a callable to be executed with the given arguments.
+
+        Schedules the callable to be executed as fn(*args, **kwargs) and returns
+        a Future instance representing the execution of the callable.
+
+        Returns:
+            A Future representing the given call.
+        """
+        kwargs = self.__add_kwargs(kwargs)
+        future = self.__executor.submit(ExecutorPool._fun_wrapper, fn, *args, **kwargs)
+        self.__futures.append(future)
+        return future
+
+    def wait_results(
+        self, timeout=None, return_when=concurrent.futures._base.ALL_COMPLETED
+    ) -> list:
+        concurrent.futures.wait(
+            self.__futures, timeout=timeout, return_when=return_when
+        )
         results: list = []
         for future in self.__futures:
             result = future.result()
@@ -23,10 +43,18 @@ class ExecutorPool:
         self.__futures.clear()
         return results
 
+    def wait(
+        self, timeout=None, return_when=concurrent.futures._base.ALL_COMPLETED
+    ) -> list:
+        warnings.warn("replaced by wait_results", DeprecationWarning)
+        return self.wait_results(timeout=timeout, return_when=return_when)
+
+    def shutdown(self, wait=True, *, cancel_futures=False):
+        self.__executor.shutdown(wait=wait, cancel_futures=cancel_futures)
+
     def stop(self) -> list:
-        results = self.wait()
-        self.__executor.shutdown()
-        return results
+        warnings.warn("replaced by shutdown", DeprecationWarning)
+        return self.shutdown()
 
     @classmethod
     def _fun_wrapper(cls, fn, *args, **kwargs):
@@ -40,12 +68,6 @@ class ExecutorPool:
             get_logger().error("catch exception:%s", e)
             get_logger().error("traceback:%s", traceback.format_exc())
             return None
-
-    def submit(self, fn: Callable, *args, **kwargs):
-        kwargs = self.__add_kwargs(kwargs)
-        future = self.__executor.submit(ExecutorPool._fun_wrapper, fn, *args, **kwargs)
-        self.__futures.append(future)
-        return future
 
     def exec(self, fn: Callable, *args, **kwargs):
         warnings.warn("replaced by submit", DeprecationWarning)
