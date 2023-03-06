aeb91aacb72b080560e63657048ed6d1ee3867a5
cyy <cyyever@outlook.com>
2023-03-06 10:31:28 +0800

    fix API

--- cyy_naive_lib/data_structure/process_initialization.py
+++ cyy_naive_lib/data_structure/process_initialization.py
@@ -1,19 +1,23 @@
 import threading
 from typing import Callable
 
+from cyy_naive_lib.log import apply_logger_setting
+
 __local_data = threading.local()
 
 
-def default_initializer(*init_args: tuple[Callable | list[Callable], dict]) -> None:
+def reinitialize_logger(__logger_setting, **kwargs):
+    apply_logger_setting(__logger_setting)
+
+
+def default_initializer(*init_args: tuple[dict]) -> None:
     # We save fun_kwargs for further processing and call the initialization function
-    __local_data.data = {}
-    if len(init_args) == 3:
-        __local_data.data = init_args[2]
-    initializers: list[Callable] = init_args[0]
-    if not isinstance(initializers, list):
-        initializers = [initializers]
+    assert len(init_args) == 1
+    init_arg_dict: dict = init_args[0]
+    initializers: list[Callable] = init_arg_dict["initializers"]
     for initializer in initializers:
-        initializer(**init_args[1])
+        initializer(**init_arg_dict["init_kwargs"])
+    __local_data.data = init_arg_dict["fun_kwargs"]
 
 
 def get_process_data() -> dict:
--- cyy_naive_lib/data_structure/process_pool.py
+++ cyy_naive_lib/data_structure/process_pool.py
@@ -1,24 +1,23 @@
 import concurrent.futures
 
-from cyy_naive_lib.log import apply_logger_setting, get_logger_setting
+from cyy_naive_lib.log import get_logger_setting
 
 from .executor_pool import ExecutorPool
-from .process_initialization import default_initializer
-
-
-def reinitialize_logger(__logger_setting, **kwargs):
-    apply_logger_setting(__logger_setting)
+from .process_initialization import default_initializer, reinitialize_logger
 
 
 class ProcessPool(ExecutorPool):
     def __init__(self, mp_context=None, initializer=None, initargs=(), **kwargs):
         if not initargs:
-            initargs = [[], {}, {}]
-        if initializer is None:
-            initargs[0] = [reinitialize_logger] + initargs[0]
-        else:
-            initargs[0] = [reinitialize_logger, initializer] + initargs[0]
-        initargs[1]["__logger_setting"] = get_logger_setting()
+            initargs = [{}]
+        if "initializers" not in initargs[0]:
+            initargs[0]["initializers"] = []
+        if initializer is not None:
+            initargs[0]["initializers"].insert(0, initializer)
+        initargs[0]["initializers"].insert(0, reinitialize_logger)
+        if "initargs" not in initargs[0]:
+            initargs[0]["initargs"] = {}
+        initargs[0]["initargs"]["__logger_setting"] = get_logger_setting()
 
         super().__init__(
             concurrent.futures.ProcessPoolExecutor(
