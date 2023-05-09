c4a41d6bd78d334fa0b53a33a66314d726f478d3
cyy <cyyever@outlook.com>
2023-05-09 12:15:31 +0800

    fix code

--- cyy_naive_lib/data_structure/process_context.py
+++ cyy_naive_lib/data_structure/process_context.py
@@ -6,29 +6,32 @@ from .mp_context import MultiProcessingContext
 
 
 class ProcessContext(MultiProcessingContext):
-    def __init__(self, use_manager: bool = False, **kwargs):
+    managers = {}
+
+    def __init__(self, ctx=multiprocessing, use_manager: bool = False, **kwargs):
+        self.__underlying_ctx = ctx
         self.__use_manager = use_manager
-        self.__manager = None
+        # self.__manager = None
         super().__init__(**kwargs)
 
-    def __getstate__(self):
-        # capture what is normally pickled
-        state = super().__getstate__()
-        state["_ProcessContext__manager"] = None
-        return state
+    # def __getstate__(self):
+    #     # capture what is normally pickled
+    #     state = super().__getstate__()
+    #     state["_ProcessContext__manager"] = None
+    #     return state
 
     def __get_ctx(self):
         ctx = self.get_manager()
         if ctx is None:
-            ctx = multiprocessing
+            ctx = self.__underlying_ctx
         return ctx
 
     def get_manager(self):
         if not self.__use_manager:
             return None
-        if self.__manager is None:
-            self.__manager = multiprocessing.Manager()
-        return self.__manager
+        if self.__underlying_ctx not in self.managers:
+            self.managers[self.__underlying_ctx] = self.__underlying_ctx.Manager()
+        return self.managers[self.__underlying_ctx]
 
     def create_queue(self) -> Any:
         return self.__get_ctx().Queue()
@@ -37,6 +40,6 @@ class ProcessContext(MultiProcessingContext):
         return self.__get_ctx().Event()
 
     def create_worker(self, name, target, args, kwargs):
-        return multiprocessing.Process(
+        return self.__underlying_ctx.Process(
             name=name, target=target, args=args, kwargs=kwargs
         )
