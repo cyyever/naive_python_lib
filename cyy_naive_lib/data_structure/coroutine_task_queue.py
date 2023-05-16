15f10944eff803a657a616ebcbabd62754ae94d9
cyy <cyyever@outlook.com>
2023-05-16 12:12:40 +0800

    fix code

--- cyy_naive_lib/data_structure/coroutine_task_queue.py
+++ cyy_naive_lib/data_structure/coroutine_task_queue.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-
 from .coroutine_context import CoroutineContext
 from .task_queue import TaskQueue
 
 
 class CoroutineTaskQueue(TaskQueue):
-    def __init__(self, **kwargs: dict):
+    def __init__(self, **kwargs: dict) -> None:
         super().__init__(mp_ctx=CoroutineContext(), **kwargs)
--- cyy_naive_lib/data_structure/worker_context.py
+++
@@ -1,6 +0,0 @@
-from typing import Any
-
-
-class MultiprocessingContext:
-    def create_queue(self) -> Any:
-        raise NotImplementedError()
