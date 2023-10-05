f17ef74a0761a67af50fed2e6bacf93c608600e5
cyy <cyyever@outlook.com>
2023-10-05 19:12:18 +0800

    fix code

--- cyy_naive_lib/test/data_structure/test_process_context.py
+++ cyy_naive_lib/test/data_structure/test_process_context.py
@@ -1,11 +1,4 @@
 from cyy_naive_lib.data_structure.process_context import ProcessContext
-from cyy_naive_lib.log import get_logger
-
-
-def hello(task, **kwargs):
-    assert task == ()
-    get_logger().info("call from other process")
-    return "abc"
 
 
 def test_pipe():
