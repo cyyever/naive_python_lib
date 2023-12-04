a7633a542cb3165ffc502277ce1e8f47d30649f3
cyy <cyyever@outlook.com>
2023-12-05 00:46:56 +0800

    Fix code

--- cyy_naive_lib/shell/shell.py
+++ cyy_naive_lib/shell/shell.py
@@ -1,56 +1,65 @@
 import subprocess
 import sys
-import tempfile
 from threading import Thread
 from time import sleep
 
 
 class Shell:
     @staticmethod
-    def exec(command_line: list, extra_output_files=None):
+    def exec(
+        command_line: list,
+        print_out: bool = True,
+        extra_output_files: list[str] | None = None,
+    ):
         r"""
         Execute a command line
         """
-        with tempfile.NamedTemporaryFile(mode="w+t", encoding="utf8") as output_file:
-            output_files = []
-            if extra_output_files is not None:
-                output_files = extra_output_files
-            output_files.append(output_file)
-            with subprocess.Popen(
-                command_line,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            ) as proc:
-                threads = [
-                    Thread(
-                        target=Shell.__output_text_line,
-                        args=(proc.stdout, [sys.stdout] + output_files),
+        output_files = []
+        if extra_output_files is not None:
+            output_files = extra_output_files
+        output_lines: list[str] = []
+        with subprocess.Popen(
+            command_line,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ) as proc:
+            threads = [
+                Thread(
+                    target=Shell.__output_text_line,
+                    args=(
+                        proc.stdout,
+                        ([sys.stdout] if print_out else []) + output_files,
+                        output_lines,
                     ),
-                    Thread(
-                        target=Shell.__output_text_line,
-                        args=(proc.stderr, [sys.stderr] + output_files),
+                ),
+                Thread(
+                    target=Shell.__output_text_line,
+                    args=(
+                        proc.stderr,
+                        ([sys.stderr] if print_out else []) + output_files,
+                        None,
                     ),
-                ]
-                for thd in threads:
-                    thd.daemon = True
-                    thd.start()
+                ),
+            ]
+            for thd in threads:
+                thd.daemon = True
+                thd.start()
 
-                while True:
-                    alive = False
-                    for thd in threads:
-                        if thd.is_alive():
-                            alive = True
-                            break
-                    if alive:
-                        sleep(0.1)
-                    else:
+            while True:
+                alive = False
+                for thd in threads:
+                    if thd.is_alive():
+                        alive = True
                         break
+                if alive:
+                    sleep(0.1)
+                else:
+                    break
 
-                exit_code = proc.wait()
-                for thd in threads:
-                    thd.join()
-                output_file.seek(0, 0)
-                return [output_file.read(), exit_code]
+            exit_code = proc.wait()
+            for thd in threads:
+                thd.join()
+            return ["\n".join(output_lines), exit_code]
 
     @staticmethod
     def __decode_output(line):
@@ -60,9 +69,11 @@ class Shell:
             return line.decode("utf-8", errors="ignore")
 
     @staticmethod
-    def __output_text_line(input_file, output_files) -> None:
+    def __output_text_line(input_file, output_files, output_lines) -> None:
         for line in iter(input_file.readline, b""):
             decoded_line = Shell.__decode_output(line)
+            if output_lines is not None:
+                output_lines.append(decoded_line)
             for f in output_files:
                 f.write(decoded_line)
                 f.flush()
