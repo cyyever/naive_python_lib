640d4215bce2bde0b17ac7445b5cd939e73f4bd5
cyy <cyyever@outlook.com>
2023-12-06 15:09:23 +0800

    Fix code

--- cyy_naive_lib/reflection.py
+++ cyy_naive_lib/reflection.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, Callable
+from typing import Any, Callable, Generator
 
 
 def get_kwarg_names(fun: Callable) -> set:
@@ -15,12 +15,19 @@ def call_fun(fun: Callable, kwargs: dict) -> Any:
     return fun(**{k: v for k, v in kwargs.items() if k in get_kwarg_names(fun)})
 
 
+def get_descendant_attrs(obj: Any, filter_fun: Callable, recursive: bool) -> Generator:
+    for name in dir(obj):
+        attr = getattr(obj, name)
+        if filter_fun(attr=attr, name=name):
+            yield name, attr
+        else:
+            yield from get_descendant_attrs(attr, filter_fun, recursive)
+
+
 def get_class_attrs(obj: Any, filter_fun: Callable | None = None) -> dict:
-    classes = {
-        name: getattr(obj, name)
-        for name in dir(obj)
-        if inspect.isclass(getattr(obj, name))
-    }
-    if filter_fun is not None:
-        classes = {k: v for k, v in classes.items() if filter_fun(k, v)}
-    return classes
+    def new_filter_fun(name: str, attr: Any) -> bool:
+        if filter_fun is None:
+            return inspect.isclass(attr)
+        return inspect.isclass(attr) and filter_fun(name, attr)
+
+    return dict(get_descendant_attrs(obj, new_filter_fun, recursive=False))
