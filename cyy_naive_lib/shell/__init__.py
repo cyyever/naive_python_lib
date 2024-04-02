3b47e8b4f7ed930d212e7ffbc9993b509f4a2893
cyy <cyyever@outlook.com>
2024-04-02 11:33:28 +0800

    Fix code

--- cyy_naive_lib/shell/__init__.py
+++ cyy_naive_lib/shell/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any, Type
 
-from ..system_info import get_operating_system
+from ..system_info import OSType, get_operating_system_type
 from .bash_script import BashScript
 from .mingw64_script import Mingw64Script  # noqa: F401
 from .msys2_script import MSYS2Script  # noqa: F401
@@ -8,19 +8,22 @@ from .pwsh_script import PowerShellScript
 from .script import Script
 
 
-def get_shell_script_type(os_hint: str | None = None) -> Type:
+def get_shell_script_type(os_hint: OSType | None = None) -> Type:
     if os_hint is None:
-        os_hint = get_operating_system()
-    if os_hint == "windows":
+        os_hint = get_operating_system_type()
+    if os_hint == OSType.Windows:
         return PowerShellScript
     return BashScript
 
 
-def get_shell_script(content: str, os_hint: str | None = None) -> Script:
+def get_shell_script(content: str, os_hint: OSType | None = None) -> Script:
     return get_shell_script_type(os_hint)(content)
 
 
 def exec_cmd(
-    cmd: str, os_hint: str | None = None, throw: bool = True, workdir: str | None = None
+    cmd: str,
+    os_hint: OSType | None = None,
+    throw: bool = True,
+    workdir: str | None = None,
 ) -> tuple[Any, int]:
     return get_shell_script(cmd, os_hint=os_hint).exec(throw=False)
