08bea6aedcea7ada20c34ea24861c7317e430c5b
cyy <cyyever@outlook.com>
2024-04-18 17:59:59 +0800

    Fix code

--- cyy_naive_lib/test/test_log.py
+++ cyy_naive_lib/test/test_log.py
@@ -2,17 +2,18 @@ import logging
 import multiprocessing
 
 from cyy_naive_lib.fs.tempdir import TempDir
-from cyy_naive_lib.log import get_logger, log_debug, log_error, log_info
+from cyy_naive_lib.log import (get_logger, log_debug, log_error, log_info,
+                               log_warning)
 
 
-def test_log():
+def test_log() -> None:
     with TempDir():
         multiprocessing.current_process().name = "my process"
         log_debug("debug msg")
         get_logger().setLevel(logging.INFO)
         log_debug("no debug msg")
         log_info("info msg")
-        get_logger().warning("warning msg")
+        log_warning("warning msg")
         log_error("error msg")
         # add_file_handler("log")
         # with open("log", "rt", encoding="utf8") as f:
