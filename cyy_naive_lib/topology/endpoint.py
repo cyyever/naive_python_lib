c2fb0db3084e9d8518dc8457b61fbb487c2e8443
cyy <cyyever@outlook.com>
2024-05-13 11:24:28 +0800

    Fix code

--- cyy_naive_lib/topology/endpoint.py
+++ cyy_naive_lib/topology/endpoint.py
@@ -4,3 +4,6 @@ from .topology import Topology
 class Endpoint:
     def __init__(self, topology: Topology) -> None:
         self._topology: Topology = topology
+
+    def close(self) -> None:
+        raise NotImplementedError()
