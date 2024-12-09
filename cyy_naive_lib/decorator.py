3238ea3b0a7c24b4945a0beb4b37e9c96315891c
cyy <cyyever@outlook.com>
2024-12-09 11:11:11 +0800

    Fix code

--- cyy_naive_lib/decorator.py
+++ cyy_naive_lib/decorator.py
@@ -9,7 +9,10 @@ class Decorator:
     def __copy__(self) -> Self:
         return type(self)(copy.copy(self._object))
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         if "object" in name:
             raise AttributeError()
         return getattr(self._object, name)
+
+    def get_underlying_object(self) -> Any:
+        return self._object
