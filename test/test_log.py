59456e229e128329a8216656cd3838f081afca5b
cyy <cyyever@outlook.com>
2025-01-28 16:45:59 +0800

    Fix deps

--- test/test_log.py
+++ test/test_log.py
@@ -5,6 +5,7 @@ from cyy_naive_lib.concurrency import ProcessPool
 from cyy_naive_lib.fs.tempdir import TempDir
 from cyy_naive_lib.log import (
     add_file_handler,
+    remove_file_handler,
     log_debug,
     log_error,
     log_info,
@@ -26,7 +27,7 @@ def test_log() -> None:
     with TempDir():
         multiprocessing.current_process().name = "my process"
         add_file_handler("log")
-        __test_log()
+        remove_file_handler("log")
         # with open("log", encoding="utf8") as f:
         #     file_content = f.readlines()
         #     log_info("file content %s", file_content)
