b5ff715103a3adf1431ebf54c99f807e3afa7617
cyy <cyyever@outlook.com>
2025-02-21 13:08:08 +0800

    Add code

--- cyy_naive_lib/storage/__init__.py
+++ cyy_naive_lib/storage/__init__.py
@@ -1,5 +1,5 @@
 from .concurrency import GlobalStore
 from .storage import SyncedDataStorage as DataStorage
-from .storage import persistent_cache
+from .storage import get_cached_data, persistent_cache
 
-__all__ = ["DataStorage", "persistent_cache","GlobalStore"]
+__all__ = ["DataStorage", "persistent_cache", "GlobalStore", "get_cached_data"]
