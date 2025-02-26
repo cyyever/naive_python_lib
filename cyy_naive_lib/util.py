e324ebbe947c08d729717e2a0328eef552a03262
cyy <cyyever@outlook.com>
2025-02-26 09:13:37 +0800

    Fix code

--- cyy_naive_lib/util.py
+++ cyy_naive_lib/util.py
@@ -1,10 +1,9 @@
 def readlines(file_path: str) -> list[str]:
-    lines = []
     try:
         with open(file_path, encoding="utf-8") as f:
-            lines += f.readlines()
+            return f.readlines()
     # pylint: disable=broad-exception-caught
     except Exception:
         with open(file_path, encoding="utf-8-sig") as f:
-            lines += f.readlines()
-    return lines
+            return f.readlines()
+    return []
