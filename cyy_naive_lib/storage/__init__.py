ee33198520ed55afa0908dff75c2e82abf5ec0d3
cyy <cyyever@outlook.com>
2025-08-23 15:54:06 +0800

    Fix code
    Signed-off-by: cyy <cyyever@outlook.com>

--- cyy_naive_lib/storage/__init__.py
+++ cyy_naive_lib/storage/__init__.py
@@ -1,5 +1,16 @@
 from .concurrency import GlobalStore
+from .json import (
+    load_json,
+    save_json,
+)
 from .storage import SyncedDataStorage as DataStorage
 from .storage import get_cached_data, persistent_cache
 
-__all__ = ["DataStorage", "persistent_cache", "GlobalStore", "get_cached_data"]
+__all__ = [
+    "DataStorage",
+    "persistent_cache",
+    "GlobalStore",
+    "get_cached_data",
+    "load_json",
+    "save_json",
+]
