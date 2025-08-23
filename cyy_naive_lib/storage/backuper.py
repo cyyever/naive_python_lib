5bc99a189a2c21fb628c466b0c55e5bfc1d1dd75
cyy <cyyever@outlook.com>
2025-08-23 16:24:04 +0800

    Fix code
    Signed-off-by: cyy <cyyever@outlook.com>

---
+++ cyy_naive_lib/storage/backuper.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Backuper:
+    def __init__(self, file: str) -> None:
+        self.file = file
+        self.__hard_link: str | None = None
+
+    def __enter__(self):
+        if not os.path.isfile(self.file):
+            return
+        for i in range(100):
+            self.__hard_link = self.file + f".__hard_link{i}"
+            if not os.path.exists(self.__hard_link):
+                os.link(self.file, self.__hard_link)
+                break
+        if self.__hard_link is None:
+            raise RuntimeError("can't backup file")
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, real_traceback):
+        if real_traceback:
+            return
+        if self.__hard_link is not None:
+            os.unlink(self.__hard_link)
---
+++ cyy_naive_lib/storage/json.py
@@ -0,0 +1,24 @@
+import contextlib
+import json
+import os
+from typing import Any
+
+from .backuper import Backuper
+
+
+def load_json(json_file: str) -> dict[str, Any]:
+    res = {}
+    if os.path.isfile(json_file):
+        with open(json_file, encoding="utf8") as f:
+            res = json.load(f)
+            assert isinstance(res, dict), json_file
+            res = {str(k): v for k, v in res.items()}
+    return res
+
+
+def save_json(data, json_file: str, backup: bool = True) -> None:
+    with (
+        Backuper(json_file) if backup else contextlib.nullcontext(),
+        open(json_file, "w", encoding="utf8") as f,
+    ):
+        json.dump(data, f, indent=2, sort_keys=True)
