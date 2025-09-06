06a9ef2fd482bc91c20176e5e59bc2e7d48e5b9c
cyy <cyyever@outlook.com>
2025-09-06 14:36:56 +0800

    More fixes
    Signed-off-by: cyy <cyyever@outlook.com>

--- cyy_naive_lib/algorithm/__init__.py
+++ cyy_naive_lib/algorithm/__init__.py
@@ -5,6 +5,7 @@ from .mapping_op import (
     flatten_mapping,
     get_mapping_items_by_key_order,
     get_mapping_values_by_key_order,
+    mapping_to_list,
     reduce_values_by_key,
 )
 from .sequence_op import (
@@ -27,5 +28,6 @@ __all__ = [
     "change_mapping_keys",
     "change_mapping_values",
     "flatten_mapping",
+    "mapping_to_list",
     "reduce_values_by_key",
 ]
